043f5f9713d28b263176e1d6ba6d3be9beaa0e65
Tech Fleet Language Project <147744718+tech-fleet-language-project@users.noreply.github.com>
2024-05-04 01:08:17 +0100

    update functions

--- Dynamic_Array.js
+++ Dynamic_Array.js
@@ -1,41 +1,64 @@
 class DynamicArray {
-    constructor(values, dynArray, sizeOfArray) {
+    constructor(values) {
         this.values = values | null;
-        this.dynArray = [typeof(this.values)];
-        this.sizeOfArray = 0;
-        this.len = dynArray.length;
+        this.sizeOfArray = this.sizeOfArrayGetter - 1;
+        this.lenOfArray = this.sizeOfArrayGetter;
+        this.thisDynArray = null;
     }
 
     // dynArray1 = new Array(5);
     // dynArray1.apply(this, Array(5)).map(() => {})
 
-    createArray() {
-        this.dynArray.fill((value = 0) => {value += 1; this.sizeOfArray += 1;});
+    // test = () => {
+    //     console.log(this.sizeOfArray);
+    // }
+
+    createArray = function() {
+        var value = 10;
+        var dynArray = new Array(value);
+        // for (var i = 0; i < 10; i++) {
+        //     dynArray[i] = i;
+        //     this.sizeOfArray += 1
+        // };
+        // dynArray.fill(value, 0 , 10);
+        // dynArray.map((value) => (new Array(value).fill(value, 0 , value)));
+        // dynArray.map(value => value--).sort();
+
+        dynArray.fill().map(() => {return Math.floor(Math.random() * value)});
+
+        this.thisDynArray = Array(value).fill().map(() => Math.floor(Math.random() * value));   
+        
     }
 
     resizeArray() {
-        this.sizeOfArray += 1;
-        this.dynArray[typeof(this.values)] * this.sizeOfArray;
-        this.dynArray = new Array(this.sizeOfArray);
+        this.lenOfArray = this.sizeOfArrayGetter +  1;
+        this.thisDynArray[typeof(this.values)] * this.lenOfArray;
+        this.thisDynArray = new Array(this.lenOfArray);
     }
 
-    insertFrontArray = (val) => {
-        copyArray = this.dynArray.fill(value)
+    insertFrontArray =(val) => {
+        
+        // var copyArray = this.thisDynArray.fill()
+        // var copyArray = this.thisDynArray.splice(0, this.thisDynArray.length - 1)
+        var copyArray = this.thisDynArray.toSpliced(0, -1);
+        // var copyArray = this.thisDynArray.map(() => this.thisDynArray.shift());
+        // var copyArray2 = this.thisDynArray.forEach(this.thisDynArray.shift());
         this.resizeArray();
 
-        for (var i = 0; i <= this.len; i++) {
-            if (i == 0) {
-                this.dynArray[i] = val;
-            }
-            else {
-                this.dynArray[i] = copyArray[i-1];
-            }
-        }
+        // for (var i = 0; i <= copyArray.length; i++) {
+        //     if (i == 0) {
+        //         this.thisDynArray[i] = val;
+        //     }
+        //     else {
+        //         this.thisDynArray[i] = copyArray[i-1];
+        //     }
+        // }
+        
+        var test3 = copyArray.filter(value => value);
+        this.thisDynArray[0] = val;
+        this.thisDynArray.fill(copyArray.unshift(), 1, this.lenOfArray);
 
-        this.dynArray[0] = val;
-        this.dynArray.fill(value, 1, length);
-
-        this.dynArray.flatMap((v) => new Array(self.len).fill(v, 0, self.len))
+        this.thisDynArray.flatMap((v) => new Array(self.lenOfArray).fill(v, 0, self.lenOfArray))
 
     }
 
@@ -63,9 +86,11 @@ class DynamicArray {
         }
     }
 
-    sizeOfArray = (function () {
-            return this.sizeOfArray;
-    })();
+    // sizeOfArray = (function () {
+    //         this.sizeOfArray;
+    // })();
+
+    get sizeOfArrayGetter() {try {return this.thisDynArray.length;} catch(err) {return 0}}
 
     get indexGetter() {
         return this.dynArray.indexOf(val);
@@ -79,9 +104,9 @@ class DynamicArray {
         this.dynArray.every(value, index => this.dynArray[index] = null);
     }
 
-    isEmpty = new function() {
-        return this.len != 0;
-    }
+    // isEmpty = new function() {
+    //     return this.len != 0;
+    // }
 
     toString = () => {
         this.dynArray = this.dynArray.every(value => String(value) )
@@ -90,13 +115,13 @@ class DynamicArray {
 
 
 
-dynamicArray = new DynamicArray();
+dynamicArray = new DynamicArray([4,4,6,7]);
 
 dynamicArray.createArray();
 dynamicArray.insertFrontArray(5);
-dynamicArray.insertBackArray(5);
-dynamicArray.insertAtIndex(5, 3);
-dynamicArray.sizeOfArray();
-dynamicArray.isEmpty();
-dynamicArray.toString();
-dynamicArray.clearArray();
+// dynamicArray.insertBackArray(5);
+// dynamicArray.insertAtIndex(5, 3);
+// dynamicArray.sizeOfArray();
+// dynamicArray.isEmpty();
+// dynamicArray.toString();
+// dynamicArray.clearArray();
